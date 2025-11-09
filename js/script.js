77144a79bc559df84c7eb33b9d076b74688ba825
Your Name <you@example.com>
2025-11-10 01:57:21 +0600

    2nd

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 let number = document.getElementById("number");
 let pinNumber = document.getElementById("pinNumber");
 let loginBtn = document.getElementById("loginBtn");
+let pageOne = document.getElementById("pageOne");
+let pageTwo = document.getElementById("pageTwo");
 let successAlert = document.getElementById("successAlert");
 let errorAlert = document.getElementById("errorAlert");
 
 loginBtn.addEventListener("click", () => {
   if (number.value == "1234" && pinNumber.value == "1234") {
-    successAlert.removeAttribute("hidden");
+    pageOne.setAttribute("hidden",true)
+    pageTwo.removeAttribute("hidden")
+
+    // successAlert.removeAttribute("hidden");
     errorAlert.setAttribute("hidden",true)
     number.value = "";
     pinNumber.value = "";
